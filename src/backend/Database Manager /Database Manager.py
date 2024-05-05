89ae0480022b7424aade6279c98e1185e89962b4
boraeskin <164562511+boraeskin@users.noreply.github.com>
2024-05-05 16:57:43 +0300

    Create Database Manager.py

---
+++ src/backend/Database Manager /Database Manager.py
@@ -0,0 +1,15 @@
+# backend-database_manager.py
+
+class DatabaseManager:
+    def __init__(self):
+        # Initialization of collections to store data
+        self.donors = []
+        self.operation_coordinators = []
+        self.system_administrators = []
+        self.volunteers = []
+        self.donations = []
+        self.shipment_requests = []
+        self.aid_registrations = []
+        self.aid_operations = []
+        self.personal_profiles = []
+
